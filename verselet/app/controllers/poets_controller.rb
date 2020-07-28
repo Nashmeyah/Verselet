096a8844fee2e3a8b692047d87b9883d9f8e946f
Nashmeyah <nashalrekabi@gmail.com>
2020-07-27 22:00:35 -0600

    Implementing new poet feature

--- verselet/app/controllers/poets_controller.rb
+++ verselet/app/controllers/poets_controller.rb
@@ -1,25 +1,30 @@
 class PoetsController < ApplicationController
   before_action :set_poet, only: [:show, :update, :destroy]
 
-  # GET /poets
   def index
     poets = Poet.all
 
     render json: poets
   end
 
-  # GET /poets/1
-  def show
-    render json: @poet
+  def create
+    poet = Poet.new(poet_params)
+    if poet.save
+      render json: poet, status: :created
+    else 
+      render json: poem.errors, status: :unprocessable_entity
+    end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
+  def show
+    render json: poet
+  end
+
+ private
     def set_poet
-      @poet = Poet.find(params[:id])
+      poet = Poet.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def poet_params
       params.require(:poet).permit(:name, :style)
     end
